538f0710eacda5bbe705d90e6bd77eac3c6883f0
alyona317 <118733159+alyona317@users.noreply.github.com>
2025-01-06 20:33:46 +0300

    Update remove-from-list.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+function removeKFromList(l, k) {
+  let dummy = new ListNode(0);
+  dummy.next = l;
+  let current = dummy;
+
+  // Traverse the list
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next; // Skip the node with value k
+    } else {
+      current = current.next; // Move to the next node
+    }
+  }
+
+  // Return the modified list, skipping the dummy node
+  return dummy.next;
+}
+
+// Helper function to print the list for easy visualization
+function printList(head) {
+  let result = [];
+  while (head) {
+    result.push(head.value);
+    head = head.next;
+  }
+  return result;
 }
 
 module.exports = {
